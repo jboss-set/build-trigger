ee7b05775c48c73346aec1254f1fb459ae299302
xstefank <xstefank122@gmail.com>
2021-06-21 16:38:16 +0200

    Add initial codebase

---
+++ src/test/java/io/xstefank/NativeBuildTriggerResourceIT.java
@@ -0,0 +1,9 @@
+package io.xstefank;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeBuildTriggerResourceIT extends BuildTriggerResourceTest {
+
+    // Execute the same tests but in native mode.
+}
