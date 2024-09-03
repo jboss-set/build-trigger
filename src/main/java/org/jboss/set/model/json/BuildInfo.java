2edebc2a91934bcdaf5fb8174b4a92913759565f
khermano <khermano@redhat.com>
2024-09-03 13:04:25 +0200

    Constraining the format of upstream input

--- src/main/java/org/jboss/set/model/json/BuildInfo.java
+++ src/main/java/org/jboss/set/model/json/BuildInfo.java
@@ -2,6 +2,7 @@ package org.jboss.set.model.json;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public class BuildInfo {
 
@@ -13,6 +14,7 @@ public class BuildInfo {
     public Environment environment;
 
     @NotNull
+    @Pattern(regexp = "^(https:\\/\\/\\S+|git\\+\\S+|git@\\S+)$", message = "Invalid format! The upstream must start with https://, git@ or git+")
     public String upstream;
 
     @NotNull
