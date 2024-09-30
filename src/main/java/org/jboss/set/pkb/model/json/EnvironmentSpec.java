0944051d383a136c80ccdad0116a909960772030
khermano <khermano@redhat.com>
2024-09-30 10:50:26 +0200

    [Build-trigger_33] Removing PKB index and unsatisfactory fronted

--- src/main/java/org/jboss/set/pkb/model/json/EnvironmentSpec.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.set.pkb.model.json;
-
-import java.util.List;
-
-public class EnvironmentSpec {
-
-    public Tools tools;
-    public List<String> requiredTools;
-}
--- src/main/java/org/jboss/set/pkb/model/json/Source.java
+++
@@ -1,7 +0,0 @@
-package org.jboss.set.pkb.model.json;
-
-public class Source {
-
-    public String internalScmUrl;
-    public String scmUrl;
-}
